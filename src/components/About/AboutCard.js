dbbf4a9161d245ef6c8211b6edc69d9d304d933f
Huy Nguyen Quang <huy.nguyen.quang@gosell.vn>
2025-02-26 15:03:44 +0700

    commit code

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,22 +9,22 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Huy Nguyen Quang </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            from <span className="purple"> Distric 07, Ho Chi Minh.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am a software developer with four years of experience in various
+            large and small projects.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have participated in many different complex projects and can work
+            independently or a keymember in a team.
             <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
+            <br />I have programming skills in many languages and technologies
+            such as VB.NET, C#, ReactJS, SQL Server, PostgresSQL, Elasticsearch,
+            RabbitMQ. I also have the ability to research and learn new
+            technologies when required or given the opportunity
           </p>
           <ul>
             <li className="about-activity">
               <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
